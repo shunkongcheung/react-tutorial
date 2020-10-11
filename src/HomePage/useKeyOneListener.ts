3a55cc1a80fe1cae8699f0101ee31e15514aac45
Shun Cheung <shunkongcheung@gmail.com>
2020-10-11 09:48:33 -0400

    feat(useKeyOneListener): init

---
+++ src/HomePage/useKeyOneListener.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+function useKeyOneListener() {
+  useEffect(() => {
+    const onKeyOneDown = (event: KeyboardEvent) => {
+      if (event.keyCode === 49)
+        alert("Key 1 is pressed! I know what you are thinking.. but don't!");
+    };
+
+    document.addEventListener("keydown", onKeyOneDown);
+
+    return () => {
+      console.log("component is unmounted");
+      document.removeEventListener("keydown", onKeyOneDown);
+    };
+  }, []);
+}
+
+export default useKeyOneListener;
