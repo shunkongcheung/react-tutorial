8542462aeef296939e2618c589b3c8e5201585d9
Shun Cheung <shunkongcheung@gmail.com>
2020-10-11 09:49:56 -0400

    feat(Home): useKeyOneListener

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -2,9 +2,12 @@ import React, { memo } from "react";
 
 import classNames from "./index.module.css";
 
+import useKeyOneListener from "./useKeyOneListener";
+
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = () => {
+  useKeyOneListener();
   return <div className={classNames.container}>Welcome HomePage.</div>;
 };
 
