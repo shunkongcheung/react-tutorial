bd96c344cfc7e1356607ef282414c2da33e644c2
Shun Kong Cheung <shcheung@microsoft.com>
2022-04-13 15:00:28 -0700

    feat(App.tsx): form init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input id="username"></input>
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password"></input>
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   );
 }
 
