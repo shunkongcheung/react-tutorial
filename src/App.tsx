7aacc9e69e22bfe48cd6be11fda5cb9c999b46d3
Shun Kong Cheung <shcheung@microsoft.com>
2022-04-13 15:06:22 -0700

    feat(App.tsx): handle form value change and submit

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import './App.css';
 function App() {
   const [username, setUsername] =  useState("");
   const [password, setPassword] =  useState("");
+
+
   return (
-    <form>
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      alert(`Hello! Mr. ${username}, I hacked your password, that is ${password}`)
+    }}>
       <div>
         <label htmlFor="username">Username</label>
-        <input id="username" value={username}></input>
+        <input id="username" value={username} onChange={(event) => setUsername(event.target.value)}></input>
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" value={password}></input>
+        <input id="password" type="password" value={password} onChange={(event) => setPassword(event.target.value)}></input>
       </div>
       <div>
         <button type="submit">Submit</button>
