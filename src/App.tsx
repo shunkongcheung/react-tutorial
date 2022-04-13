f22f384c02d2e4400fd7063d43256864a374965e
Shun Kong Cheung <shcheung@microsoft.com>
2022-04-13 15:03:40 -0700

    feat(App.tsx): form value

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [username, setUsername] =  useState("");
+  const [password, setPassword] =  useState("");
   return (
     <form>
       <div>
         <label htmlFor="username">Username</label>
-        <input id="username"></input>
+        <input id="username" value={username}></input>
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input id="password" type="password"></input>
+        <input id="password" type="password" value={password}></input>
       </div>
       <div>
         <button type="submit">Submit</button>
